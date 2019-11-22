6239024ae436ef57a62bafceec436d95680963d0
ruchitGandhiNEU <55202985+ruchitGandhiNEU@users.noreply.github.com>
2019-11-22 03:27:25 -0500

    Setting up basic skeleton for application route

---
+++ nodeAPI/api/routes/application-route.js
@@ -0,0 +1,14 @@
+/**
+ * Application endpoint route definitions.
+ */
+
+'use strict';
+
+/**
+ * exports the initialization of the api to handle and route the incoming requests
+ *
+ * @param expressApp
+ */
+module.exports = function (expressApp) {
+
+};
