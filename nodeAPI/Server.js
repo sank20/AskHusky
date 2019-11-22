b92041382e813309a7c10f8c0c28129a58aafdf6
Darshan Ashwin Dedhia <dedhia.da@husky.neu.edu>
2019-11-22 01:09:15 -0500

    Created initial skeleton of the NodeAPI project

---
+++ nodeAPI/Server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+const port = process.env.port || 3000;
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb+srv://mongo-db-user:Asd1234@mongocluster-2bbjv.mongodb.net/test?retryWrites=true&w=majority', {
+    useMongoClient: true
+});
+
+mongoose.Promise = global.Promise;
+
+app.use(bodyParser.urlencoded({
+    extended : true
+}));
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+res.header("Access-Control-Allow-Origin", "*");
+res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+res.header("Access-Control-Allow-Methods", "*");
+next();
+});
+
+let initApp =require('./api/app');
+initApp(app);
+app.listen(port);
+console.log("Server for ToDo List listening on port: " + port);
